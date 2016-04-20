69880145b3e830f439c9b3dae98e2bff22afffa0
Manuel Eberhardinger <m.eberhardinger@yahoo.de>
2016-04-20 14:05:25 +1200

    added exception for passwords in edit forms

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   has_many :time_trackings, dependent: :destroy
   mount_uploader :picture, PictureUploader
   validates :picture, file_size: { less_than_or_equal_to: 2.megabytes },
